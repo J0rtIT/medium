dbf4cb744681b40573d2cff8b205ab78bce9a670
Jose Gabriel Ortega Castro <jortega@faboit.com>
2021-08-30 07:33:15 -0500

    Updates

--- How to enable International and Domestic Calls for O365 users if your live out of the US/Change-Location.ps1
+++ How to enable International and Domestic Calls for O365 users if your live out of the US/Change-Location.ps1
@@ -1,7 +1,7 @@
 ##
 # Medium URL: https://j0rt3g4.medium.com/how-to-change-the-location-of-an-ms-o365-user-e55018fd9617
 # GITHUB Repository: https://github.com/j0rt3g4/medium/blob/master/How%20to%20enable%20International%20and%20Domestic%20Calls%20for%20O365%20users%20if%20your%20live%20out%20of%20the%20US/Change-Location.ps1
-# YOUTUBE - Schedulled
+# YOUTUBE - Scheduled
 
 
 Set-executionpolicy remoteSigned
@@ -20,6 +20,3 @@ Get-MsolUser -UserPrincipalName <upn> | Set-MsolUser -UsageLocation US
 
 #Validate the change
 Get-MsolUser -UserPrincipalName dummy@faboit.com | select UserPrincipalName,UsageLocation
-Get-MsolUser -UserPrincipalName <upn> | Set-MsolUser -UsageLocation US
-
-
