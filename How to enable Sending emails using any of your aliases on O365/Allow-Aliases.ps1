96d0b10a9c58e3ac917ec8232b156a305fdf9a3d
Jose Gabriel Ortega Castro <jortega@faboit.com>
2021-09-11 03:34:45 -0500

    Latest Updates

--- How to enable Sending emails using any of your aliases on O365/Allow-Aliases.ps1
+++ How to enable Sending emails using any of your aliases on O365/Allow-Aliases.ps1
@@ -1,36 +1,40 @@
 ##  The Art of Managing who Can Create Groups in a medium to large O365 tenant
-# Medium URL:        https://j0rt3g4.medium.com/unsplash-api-powershell-8214cfc4c87b
-# GITHUB Repository: https://github.com/j0rt3g4/medium/blob/d7e5069f02b31e4da27d240319802a8237129d61/OAuth%202.0%20with%20Powershell%20for%20Unsplash%20API%20bearer%C2%A0token/Do-Oauth20forUnsplash.ps1
+# Medium URL:        https://j0rt3g4.medium.com/how-to-enable-sending-emails-using-any-of-your-aliases-on-o365-a3f410eabbb
+# GITHUB Repository: https://github.com/j0rt3g4/medium/blob/b08fbe24dd3b72453019f0c0d1f1444c21acc570/How%20to%20enable%20Sending%20emails%20using%20any%20of%20your%20aliases%20on%20O365/Allow-Aliases.ps1
 # YOUTUBE            https://youtu.be/xxXtT648ZeI
+# RecordIT: When disabled: https://recordit.co/rQwD4Pz9Po / When is already enabled: http://recordit.co/zvXcpb24TV
 
 
 #set up executionpolicy
 Write-Host -ForegroundColor Green "Setting up Powershell Execution Policy"
 Set-executionPolicy Remotesigned
 
-#Install EXO PS Module
-Write-Host -ForegroundColor Green "Install EXchange Online (EXO) PowerShell (PS) Module"
-Install-Module ExchangeOnlineManagement -AllowClobber -force
+#Check or install EXO PS Module
 
-#connect EXO module to local computer
-Write-Host -ForegroundColor Green "Connecting to EXO PS Module, please add your Global Admin Credentials"
-Connect-ExchangeOnline | Out-Null
+Write-Host -ForegroundColor Green "Trying to connect to EXO PS Module, please add your Global Admin Credentials"
+try{
+	Connect-ExchangeOnline -ea Stop | Out-Null
+}
+Catch{
+	Write-Host -ForegroundColor Yellow "That tried failed, but dont worry We'd install the required module now and retry"
+	Install-Module ExchangeOnlineManagement -AllowClobber -force
 
+	#connect EXO module to local computer
+	Write-Host -ForegroundColor Green "Connecting to EXO PS Module, please add your Global Admin Credentials"
+	Connect-ExchangeOnline | Out-Null
+}
 
 Write-Host -ForegroundColor Magenta "Validating Property"
 $OC= Get-OrganizationConfig | select SendFromAliasEnabled
 
 if($OC.SendFromAliasEnabled){
 	Write-Host -ForegroundColor Green "Your tenant was already enabled so no changes needed to be done"
-	exit -1;
 }
 else{
 	Write-Host -ForegroundColor Magenta "Setting up the property to true."
 	Set-OrganizationConfig -SendFromAliasEnabled $true
 }
 
-
-
-Write-Host -ForegroundColor Cyan "Please validate on your own if the property SendFromAliasEnabled was set to ""TRUE"" "
+Write-Host -ForegroundColor Cyan "Please validate on your own if the property SendFromAliasEnabled was/is ""TRUE"" "
 Get-OrganizationConfig | select SendFromAliasEnabled
 
